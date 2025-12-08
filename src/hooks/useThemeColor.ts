57b3e58072541059fb512603bfc591fd05626325
서지유 <jihoseo2008@gmail.com>
2025-12-08 17:30:00 +0900

    feat :: system color

---
+++ src/hooks/useThemeColor.ts
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+export function useThemeColor(color: string) {
+    useEffect(() => {
+        let meta = document.querySelector('meta[name="theme-color"]')
+
+        if (!meta) {
+            meta = document.createElement("meta")
+            meta.setAttribute("name", "theme-color")
+            document.head.appendChild(meta)
+        }
+
+        meta.setAttribute("content", color)
+    }, [color])
+}
