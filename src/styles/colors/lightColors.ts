608359e783ca1e367cd1aa1b7a0d28bcca69f84f
서지유 <jihoseo2008@gmail.com>
2025-12-09 22:16:32 +0900

    feat :: edit color system

--- src/styles/colors/lightColors.ts
+++ src/styles/colors/lightColors.ts
@@ -1,16 +1,16 @@
 export const lightColors = {
     Surface: "#FFFFFF",
     Main: {
-        PrimaryContainer: "#F5FAFE",
-        Main100: "#C2DEFC",
-        Main200: "#9DC9FA",
-        PrimaryFixed: "#7CB3F8",
-        Main400: "#69A3F8",
-        Primary: "#5C94F7",
-        Main600: "#598CF7",
-        Main700: "#5482F6",
-        Main800: "#5078F6",
-        OnPrimary: "#4967F5",
+        PrimaryContainer: "#F3FAFF",
+        Main100: "#B3DFFF",
+        Main200: "#80CAFF",
+        PrimaryFixed: "#4DB4FF",
+        Main400: "#26A4FF",
+        Primary: "#0094FF",
+        Main600: "#008CFF",
+        Main700: "#0081FF",
+        Main800: "#0077FF",
+        OnPrimary: "#0065FF",
     },
     Gray: {
         Secondary: "#FBFBFC",
@@ -22,7 +22,7 @@ export const lightColors = {
         SurfaceContainerLow: "#848287",
         SurfaceContainer: "#79777C",
         SurfaceContainerHighest: "#6F6D72",
-        SurfaceContainerHigh: "#5C5A5F",
+        SurfaceContainerHigh: "#5C5A60",
     },
     System: {
         InverseSurface: "#000000",
@@ -30,9 +30,9 @@ export const lightColors = {
     },
     Sub: {
         OnTertiary: "#F5FEF2",
-        Tertiary: "#7AAB60",
-        onError: "#FDF7F7",
-        Error: "#DE4E3F",
+        Tertiary: "#62AE55",
+        onError: "#FFF7F7",
+        Error: "#FF3B32",
     },
 }
 
