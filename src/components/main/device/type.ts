427277646d9747b268a115d7159043d0d2b7b667
서지유 <jihoseo2008@gmail.com>
2025-12-11 10:56:11 +0900

    feat :: layout type

---
+++ src/components/main/device/type.ts
@@ -0,0 +1,29 @@
+export type DeviceType = "WASH" | "DRY"
+
+export interface DeviceData {
+    id: number
+    type: DeviceType
+    state: 0 | 1 | 2 | 3
+}
+
+export interface DeviceRef {
+    id: number
+    deviceType: DeviceType
+}
+
+export interface SingleCell {
+    type: "single"
+    device: DeviceRef
+}
+
+export interface PairCell {
+    type: "pair"
+    devices: [DeviceRef, DeviceRef]
+}
+
+export interface EmptyCell {
+    type: "empty"
+}
+
+export type LayoutCell = SingleCell | PairCell | EmptyCell
+export type LayoutRow = LayoutCell[]
