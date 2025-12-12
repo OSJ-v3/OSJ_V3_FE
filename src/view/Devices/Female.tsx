96057eac73390fb68a13560f5e47b4afed5191df
서지유 <jihoseo2008@gmail.com>
2025-12-12 11:29:38 +0900

    feat :: device layout components

---
+++ src/view/Devices/Female.tsx
@@ -0,0 +1,31 @@
+import { DeviceLayout } from "../../components/main/device/DeviceLayout"
+import type { DeviceData } from "../../components/main/device/type"
+import { femaleLayout } from "../../layouts/female"
+
+export function Female() {
+    const femaleDummyDevices: DeviceData[] = femaleLayout
+        .flat()
+        .filter((c) => c.type !== "empty")
+        .flatMap((cell) => {
+            if (cell.type === "single") {
+                return [
+                    {
+                        id: cell.device.id,
+                        type: cell.device.deviceType,
+                        state: Math.floor(Math.random() * 4) as 0 | 1 | 2 | 3,
+                    },
+                ]
+            }
+            return cell.devices.map((d) => ({
+                id: d.id,
+                type: d.deviceType,
+                state: Math.floor(Math.random() * 4) as 0 | 1 | 2 | 3,
+            }))
+        })
+
+    return (
+        <>
+            <DeviceLayout layout={femaleLayout} devices={femaleDummyDevices} />
+        </>
+    )
+}
