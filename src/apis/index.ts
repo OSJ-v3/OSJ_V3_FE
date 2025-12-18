2a666eed8eb60bbb9493484e82331ad3bf9c4380
seeum0507 <kwonstella0507@gmail.com>
2025-12-18 23:34:39 +0900

    feat :: make basic api

---
+++ src/apis/index.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_BASEURL,
+  timeout: 10000,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
