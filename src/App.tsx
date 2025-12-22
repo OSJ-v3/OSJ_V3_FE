f831b5a18d664c8faa29a71df9927ebcf1a3da71
seeum0507 <kwonstella0507@gmail.com>
2025-12-22 10:10:49 +0900

    fix :: modify route

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,64 @@
-import { useEffect } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { ThemeProvider } from "styled-components"
-import { AlarmRenderer, Splash, ToastRenderer } from "./components"
-import { AlarmProvider, ToastProvider } from "./contexts"
-import { useSystemTheme, useThemeColor } from "./hooks"
-import { useThemeStore } from "./stores"
-import { darkTheme, lightTheme, GlobalStyle, AppLayout } from "./styles"
-import { Main, Setting, Complain, Detail, Notice } from "./view"
-import { useNetworkListener } from "./hooks/useNetworkListener"
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { AlarmRenderer, Splash, ToastRenderer } from "./components";
+import { AlarmProvider, ToastProvider } from "./contexts";
+import { useSystemTheme, useThemeColor } from "./hooks";
+import { useThemeStore } from "./stores";
+import { darkTheme, lightTheme, GlobalStyle, AppLayout } from "./styles";
+import { Main, Setting, Complain, Detail, Notice } from "./view";
+import { useNetworkListener } from "./hooks/useNetworkListener";
 
 function App() {
-    useNetworkListener()
+  useNetworkListener();
 
-    useEffect(() => {
-        if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.getRegistrations().then((regs) => {
-                regs.forEach((reg) => reg.update())
-            })
-        }
-    }, [])
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistrations().then((regs) => {
+        regs.forEach((reg) => reg.update());
+      });
+    }
+  }, []);
 
-    const { mode } = useThemeStore()
-    const systemTheme = useSystemTheme()
+  const { mode } = useThemeStore();
+  const systemTheme = useSystemTheme();
 
-    const appliedTheme =
-        mode === "system"
-            ? systemTheme === "dark"
-                ? darkTheme
-                : lightTheme
-            : mode === "dark"
-            ? darkTheme
-            : lightTheme
+  const appliedTheme =
+    mode === "system"
+      ? systemTheme === "dark"
+        ? darkTheme
+        : lightTheme
+      : mode === "dark"
+      ? darkTheme
+      : lightTheme;
 
-    useThemeColor(appliedTheme.colors.Surface)
+  useThemeColor(appliedTheme.colors.Surface);
 
-    return (
-        <BrowserRouter>
-            <ThemeProvider theme={appliedTheme}>
-                <AlarmProvider>
-                    <AlarmRenderer />
-                    <ToastProvider>
-                        <Splash />
-                        <GlobalStyle />
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={appliedTheme}>
+        <AlarmProvider>
+          <AlarmRenderer />
+          <ToastProvider>
+            <Splash />
+            <GlobalStyle />
 
-                        <ToastRenderer />
+            <ToastRenderer />
 
-                        <AppLayout>
-                            <Routes>
-                                <Route path="/" element={<Main />} />
-                                <Route path="/notice" element={<Notice />} />
-                                <Route path="/setting" element={<Setting />} />
-                                <Route
-                                    path="/complain"
-                                    element={<Complain />}
-                                />
-                                <Route
-                                    path="/notice/:id"
-                                    element={<Detail />}
-                                />
-                            </Routes>
-                        </AppLayout>
-                    </ToastProvider>
-                </AlarmProvider>
-            </ThemeProvider>
-        </BrowserRouter>
-    )
+            <AppLayout>
+              <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/notice" element={<Notice />} />
+                <Route path="/setting" element={<Setting />} />
+                <Route path="/complain" element={<Complain />} />
+                <Route path="/notice/:id" element={<Detail />} />
+              </Routes>
+            </AppLayout>
+          </ToastProvider>
+        </AlarmProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
