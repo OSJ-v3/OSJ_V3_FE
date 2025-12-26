59a618cb488f125109e7dcab79ade100e3fc9f1f
seeum0507 <kwonstella0507@gmail.com>
2025-12-26 16:17:38 +0900

    feat :: fcm

--- src/hooks/useFCM.ts
+++ src/hooks/useFCM.ts
@@ -1,21 +1,18 @@
 import { getToken, onMessage } from "firebase/messaging";
 import { messaging } from "../firebase/settingFCM";
+import { useFCMStore } from "../stores/fcmStore";
 
 export const initFCM = async () => {
   console.log("initFCM 실행됨");
   console.log("알림 권한 상태:", Notification.permission);
-
   const permission = await Notification.requestPermission();
-  if (permission !== "granted") {
-    console.log("알림 권한 거부:", permission);
-    return;
-  }
+  if (permission !== "granted") return;
 
   const token = await getToken(messaging, {
     vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
   });
 
-  console.log("FCM TOKEN:", token);
+  useFCMStore.getState().setToken(token);
 
   onMessage(messaging, (payload) => {
     console.log("포그라운드 알림:", payload);
