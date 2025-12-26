cbb81ec6882f7c4a533d5bebf0b8b6e3279dfc04
seeum0507 <kwonstella0507@gmail.com>
2025-12-26 18:50:12 +0900

    feat :: yarn build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.101nqg2p2ig"
+    "revision": "0.gf7opqe7cvg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
