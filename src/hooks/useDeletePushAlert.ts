1f9999d21bd28bc2600af66b4936d0b5204adb6f
seeum0507 <kwonstella0507@gmail.com>
2026-01-06 19:27:26 +0900

    feat :: delete alerts

--- src/hooks/useDeletePushAlert.ts
+++ src/hooks/useDeletePushAlert.ts
@@ -1,7 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deletePushAlert } from "../apis/pushAlerts";
 
-export const useDeletePushAlert = () =>
-  useMutation({
+export const useDeletePushAlert = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
     mutationFn: deletePushAlert,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["push-alerts", variables.token],
+      });
+    },
   });
+};
