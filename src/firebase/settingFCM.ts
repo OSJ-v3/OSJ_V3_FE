0a8350da7cae26639e18f5ae452e79ff03c0e833
seeum0507 <kwonstella0507@gmail.com>
2026-01-06 19:26:43 +0900

    fix :: delete messaging at setting

--- src/firebase/settingFCM.ts
+++ src/firebase/settingFCM.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,4 +10,3 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const messaging = getMessaging(app);
