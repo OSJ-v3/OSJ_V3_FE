283b2bb7a5863bc7eacf5a2c0f6b61bed2e3d87c
seeum0507 <kwonstella0507@gmail.com>
2026-01-17 20:40:13 +0900

    fix :: debug pushAlerts and define byte

--- src/apis/pushAlerts/index.ts
+++ src/apis/pushAlerts/index.ts
@@ -1,40 +1,42 @@
 import { api } from "../index";
-import type { PushAlertRequest, PushAlertItem } from "./type";
+
+export interface PushAlertRequest {
+  id: number;
+  token: string;
+  expectState: number;
+}
+
+export interface PushAlertItem {
+  id: number;
+  deviceId: number;
+  expectState: number;
+}
 
 export const createPushAlert = async (data: PushAlertRequest) => {
-  const res = await api.post("/push-alerts", data);
+  const res = await api.post("/push-alerts", data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   return res.data;
 };
 
-export const deletePushAlert = async (data: { id: number; token: string }) => {
-  const res = await api.delete("/push-alerts", { data });
+export const deletePushAlert = async (data: PushAlertRequest) => {
+  const res = await api.delete("/push-alerts", {
+    data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   return res.data;
 };
 
 export const getPushAlertList = async (token: string) => {
-  if (!token) {
-    throw new Error("FCM token is required");
-  }
+  const res = await api.get("/push-alerts/list", {
+    params: { token },
+  });
 
-  console.log("알림 목록 조회 요청 - 토큰:", token.substring(0, 30) + "...");
-
-  try {
-    const res = await api.get<PushAlertItem[]>(`/push-alerts/list`, {
-      params: { token },
-    });
-
-    console.log("알림 목록 조회 성공:", res.data);
-    return res.data;
-  } catch (error: any) {
-    console.error("알림 목록 조회 실패:", {
-      status: error.response?.status,
-      statusText: error.response?.statusText,
-      data: error.response?.data,
-      config: {
-        url: error.config?.url,
-        params: error.config?.params,
-      },
-    });
-    throw error;
-  }
+  return res.data;
 };
