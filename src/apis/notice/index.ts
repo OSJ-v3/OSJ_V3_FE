6816a672e7c567824beb94ee3a31bd701a939f7c
서지유 <jihoseo2008@gmail.com>
2026-01-20 19:25:58 +0900

    feat :: notice alarm api

--- src/apis/notice/index.ts
+++ src/apis/notice/index.ts
@@ -1,7 +1,7 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import type { AxiosError } from "axios"
 import { instance } from ".."
-import type { NoticeResponse } from "./type"
+import type { NoticeAlertRequest, NoticeResponse } from "./type"
 
 export const useNotices = () => {
     return useQuery<NoticeResponse[], AxiosError>({
@@ -21,5 +21,26 @@ export const useNoticeDetail = (id: number) => {
             return res.data
         },
         enabled: !!id,
+    })
+}
+
+export const useGetNoticePushStatus = () => {
+    return useQuery<NoticeAlertRequest, AxiosError>({
+        queryKey: ["noticePushStatus"],
+        queryFn: async () => {
+            const res = await instance.get<NoticeAlertRequest>(
+                "/notices/push-alerts",
+            )
+            return res.data
+        },
+    })
+}
+
+export const useRegisterNoticePush = () => {
+    return useMutation<void, AxiosError, string>({
+        mutationFn: (token: string) =>
+            instance.post("/notices/push-alerts", null, {
+                params: { token },
+            }),
     })
 }
