97ce11d05ce47c964ed50e107411ca5e59afd0cc
서지유 <jihoseo2008@gmail.com>
2026-01-23 22:26:35 +0900

    feat :: App inner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
-import { AlarmProvider, ToastProvider } from "./contexts"
+import { ToastProvider } from "./contexts"
 import { useSystemTheme, useThemeColor } from "./hooks"
 import { useThemeStore } from "./stores"
 import { darkTheme, lightTheme, GlobalStyle, AppLayout } from "./styles"
@@ -9,12 +9,10 @@ import { useNetworkListener } from "./hooks/useNetworkListener"
 import { AlarmRenderer, Splash, ToastRenderer } from "./components/common"
 import { useInitNoticePush } from "./hooks/useInitNoticePush"
 import { useSyncAlarmFromServer } from "./hooks/useSyncAlarmFromServer"
+import { useEffect } from "react"
+import { initFCM } from "./firebase/initFCM"
 
 function App() {
-    useInitNoticePush()
-    useSyncAlarmFromServer()
-    useNetworkListener()
-
     const { mode } = useThemeStore()
     const systemTheme = useSystemTheme()
 
@@ -29,34 +27,43 @@ function App() {
 
     useThemeColor(appliedTheme.colors.Surface)
 
+    useEffect(() => {
+        initFCM()
+    }, [])
+
     return (
         <BrowserRouter>
             <ThemeProvider theme={appliedTheme}>
-                <AlarmProvider>
-                    <AlarmRenderer />
-                    <ToastProvider>
-                        <Splash />
-                        <GlobalStyle />
-                        <ToastRenderer />
-                        <AppLayout>
-                            <Routes>
-                                <Route path="/" element={<Main />} />
-                                <Route path="/notice" element={<Notice />} />
-                                <Route path="/setting" element={<Setting />} />
-                                <Route
-                                    path="/complain"
-                                    element={<Complain />}
-                                />
-                                <Route
-                                    path="/notice/:id"
-                                    element={<Detail />}
-                                />
-                            </Routes>
-                        </AppLayout>
-                    </ToastProvider>
-                </AlarmProvider>
+                <ToastProvider>
+                    <AppInner />
+                </ToastProvider>
             </ThemeProvider>
         </BrowserRouter>
+    )
+}
+
+function AppInner() {
+    useInitNoticePush()
+    useSyncAlarmFromServer()
+    useNetworkListener()
+
+    return (
+        <>
+            <Splash />
+            <GlobalStyle />
+            <AlarmRenderer />
+            <ToastRenderer />
+
+            <AppLayout>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/notice" element={<Notice />} />
+                    <Route path="/setting" element={<Setting />} />
+                    <Route path="/complain" element={<Complain />} />
+                    <Route path="/notice/:id" element={<Detail />} />
+                </Routes>
+            </AppLayout>
+        </>
     )
 }
 
