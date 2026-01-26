34fb25209bb9bcb5af104cf596a5b2d17424d87d
서지유 <jihoseo2008@gmail.com>
2026-01-26 17:15:34 +0900

    remove :: folder

--- src/apis/device.ts
+++ src/apis/device.ts
@@ -1,4 +1,4 @@
-import { instance } from ".."
+import { instance } from "."
 
 export async function fetchPushAlertList(token: string) {
     const res = await instance.get<number[]>("/push-alerts/list", {
