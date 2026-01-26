70acafda3c2b31fdb081bd3b027668ac773a7c02
서지유 <jihoseo2008@gmail.com>
2026-01-26 15:54:53 +0900

    remove :: init FCM

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,16 @@ import { createRoot } from "react-dom/client"
 import App from "./App"
 import "./styles/global.css"
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
-import { initFCM } from "./firebase/initFCM"
 import { initFCMTokenIfNeeded } from "./firebase/initFCMToken"
 import { listenForegroundMessage } from "./firebase/fcm"
 
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js", {
+    window.addEventListener("load", async () => {
+        await navigator.serviceWorker.register("/firebase-messaging-sw.js", {
             scope: "/",
-            updateViaCache: "none",
         })
     })
 }
-
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
@@ -27,7 +24,6 @@ const queryClient = new QueryClient({
 
 async function bootstrap() {
     await initFCMTokenIfNeeded()
-    await initFCM()
     await listenForegroundMessage()
 }
 
