fcbb2bb8c26e0eb688de3b012146d8736f882e1a
서지유 <jihoseo2008@gmail.com>
2026-01-27 19:43:01 +0900

    feat :: is loading

--- src/stores/useStartStore.ts
+++ src/stores/useStartStore.ts
@@ -3,19 +3,28 @@ import { persist } from "zustand/middleware"
 
 export type PageType = "mine" | "status"
 
-interface StartPage {
+interface StartPageState {
     start: PageType
+    isLoading: boolean
     setStart: (page: PageType) => void
+    setLoading: (loading: boolean) => void
 }
 
-export const useStartStore = create<StartPage>()(
+export const useStartStore = create<StartPageState>()(
     persist(
         (set) => ({
             start: "mine",
+            isLoading: true,
+
             setStart: (page) => set({ start: page }),
+            setLoading: (loading) => set({ isLoading: loading }),
         }),
         {
             name: "area-storage",
-        }
-    )
+
+            partialize: (state) => ({
+                start: state.start,
+            }),
+        },
+    ),
 )
